b53a00a65eadfc90438d4080c2cc352b40515ce7
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2026-02-06 17:20:25 +0530

    fix: correct indentation in SVG element in Navbar component

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
-                <g
+                  <g
                   strokeLinejoin="round"
                   strokeLinecap="round"
                   strokeWidth="2.5"
